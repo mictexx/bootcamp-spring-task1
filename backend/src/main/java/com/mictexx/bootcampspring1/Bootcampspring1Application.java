2bfc514c7dfa32470049aa35c0a444212dc2abc3
Michel Teixeira <teixeira.michel@gmail.com>
2022-03-08 13:48:26 -0300

    Backend project created

---
+++ backend/src/main/java/com/mictexx/bootcampspring1/Bootcampspring1Application.java
@@ -0,0 +1,13 @@
+package com.mictexx.bootcampspring1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bootcampspring1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Bootcampspring1Application.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/mictexx/bootcampspring1/Bootcampspring1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mictexx.bootcampspring1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Bootcampspring1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
